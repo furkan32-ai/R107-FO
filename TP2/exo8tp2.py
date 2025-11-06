c09610dfc96756ef04606cc5cde4035107a69fde
e2501613 <furkan.ozdemir1@outlook.fr>
2025-11-06 16:46:12 +0100

    maj

--- TP2/exo8tp2.py
+++ TP2/exo8tp2.py
@@ -3,4 +3,3 @@ if ( x>=2 and x < 3 ) or (x > 0 and x <= 1 ) or (x>=1) or (x >= -10 and x<= -2):
     print('x appartient a I')
 else:
     print(" n'appartient pas a I")
-    print(" n'appartient pas a I")
